359bd770515cdea4ca012f4cf96f785cb32220e8
Dennis Abad <dennis.abad@mindzie.com>
2021-03-17 20:23:21 +0800

    Added controllers and views
    Signed-off-by: Dennis Abad <dennis.abad@mindzie.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+
+  get '/articles' => 'articles#index'
+  
+  get '/article/new' => 'articles#new', as: 'new_article'
+  post '/article/create' => 'articles#create', as: 'create_article'
+  get 'articles/edit'
+  get 'articles/update'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
