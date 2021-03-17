31e1078f010e8c63d3c067e4fd59206b8e5b4543
Dennis Abad <dennis.abad@mindzie.com>
2021-03-17 18:36:13 +0800

    Added Article Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_124611) do
+ActiveRecord::Schema.define(version: 2021_03_17_103111) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
